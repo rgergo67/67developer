93f502cbea73d46847c432fbdd52c6e966e0d7ee
Ratting Gergely <ratting.gergo@uni-pannon.hu>
2022-03-19 14:17:51 +0100

    cover

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -12,6 +12,10 @@
     <meta property="og:url" content="{{ request()->fullUrl() }}">
     <meta property="og:type" content="article">
 
+    <meta property="og:title" content="{{ __('67developer programozás') }}">
+    <meta property="og:description" content="{{ __('Elavult, lassú, hibásan működő weboldalakat és rendszereket javítok, teszek ismét korszerűvé és villámgyorssá.') }}">
+    <meta property="og:image" content="{{ asset('images/fb_cover.jpg') }}">
+
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
